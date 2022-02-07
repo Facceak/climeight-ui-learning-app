823cbb589c817ab3cc3acd829c8cf9fbce1073d8
Facce Ak <facceak@gmail.com>
2022-02-07 10:20:18 +0100

    Components sortierung.

---
+++ app-pages/registration.jsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import OffersButton from "../assets/components/OffersButton"
+
+export default function Registration() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.modal}></View>
+      <Text style={styles.climeight}>climeight.</Text>
+      <OffersButton style={styles.btn} btntext={"JETZT REGISTRIEREN"}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    overflow: "hidden",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modal: {
+    position: "absolute",
+    bottom: -30,
+    width: "100%",
+    height: "95%",
+    borderRadius: 30,
+    backgroundColor: "black"
+  },
+  climeight: {
+    position: "absolute",
+    left: 20,
+    top: 30,
+    color: "black",
+    fontSize: 22,
+    fontWeight: 700,
+  }
+});
