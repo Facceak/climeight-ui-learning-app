20222d2cc4c3398443c50397e275ec098e334108
Facce Ak <facceak@gmail.com>
2022-02-07 10:20:46 +0100

    Components sort

--- App.js
+++ App.js
@@ -1,39 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import OffersButton from './assets/components/OffersButton';
+import Registration from './app-pages/registration';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.modal}></View>
-      <Text style={styles.climeight}>climeight.</Text>
-      <OffersButton btntext={"JETZT REGISTRIEREN"}/>
+      <Registration></Registration>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    overflow: "hidden",
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  modal: {
-    position: "absolute",
-    bottom: -30,
-    width: "100%",
-    height: "95%",
-    borderRadius: 30,
-    backgroundColor: "black"
-  },
-  climeight: {
-    position: "absolute",
-    left: 20,
-    top: 30,
-    color: "black",
-    fontSize: 22,
-    fontWeight: 700,
-  }
+
 });
--- assets/components/OffersButton.jsx
+++ assets/components/OffersButton.jsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'react-native-svg';
 
 export default function OffersButton({btntext}) {
     return (
+        <LinearGradient colors={[' #ed83f2', ' #dafc74', ' #ed4a7b']}
+        style={styles.linearGradient}> 
             <View style={styles.button_bg}>
                 <Text style={styles.btnvalue}>{btntext}</Text>
             </View>
+        </LinearGradient>
+        
     );
   };
 
@@ -20,13 +25,16 @@ const styles = StyleSheet.create({
     },
     btnvalue: {
         width: "100%",
-        height: "100%",
+        height: 800,
         justifyContent: "center",
         textAlign: "center",
         fontWeight: 600,
         fontSize: 20,
         color: "black",
         
+    },
+    linearGradient: {
+
     }
 
 });
