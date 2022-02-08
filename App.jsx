03a5c0e7c6af749ac541f36d23da9f270467d22f
Facce Ak <facceak@gmail.com>
2022-02-08 10:12:54 +0100

    Create registration form

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Registration from './app-pages/registration';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.subtitle}>climeight.</Text>
       <Registration></Registration>
       <StatusBar style="auto" />
     </View>
@@ -11,5 +13,19 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    overflow: "hidden",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },  
+  subtitle: {
+    position: "absolute",
+    left: "7%",
+    top: "3%",
+    fontSize: 18,
+    fontWeight: 600,
+    color: "black"
+  }
 });
--- app-pages/registration.jsx
+++ app-pages/registration.jsx
@@ -1,40 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 import OffersButton from "../assets/components/OffersButton"
 
 export default function Registration() {
   return (
-    <View style={styles.container}>
-      <View style={styles.modal}></View>
-      <Text style={styles.climeight}>climeight.</Text>
-      <OffersButton style={styles.btn} btntext={"JETZT REGISTRIEREN"}/>
+    <View style={styles.anmeldemodal}>
+      <View style={styles.regform}>
+        <View style={styles.email}>
+          <Text style={styles.reglabel}>E-Mail Adresse:</Text>
+          <TextInput style={styles.emailinp}/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    overflow: "hidden",
-    flex: 1,
-    backgroundColor: '#fff',
+
+  anmeldemodal: {
+    width: "100%",
+    height: "100%",
+    top: "7%",
+    borderRadius: 30,
+    backgroundColor: "black",
     alignItems: 'center',
     justifyContent: 'center',
   },
-  modal: {
-    position: "absolute",
-    bottom: -30,
-    width: "100%",
-    height: "95%",
+  regform: {
+    width: "80%",
+    height: "70%",
     borderRadius: 30,
-    backgroundColor: "black"
+    backgroundColor: "white"
   },
-  climeight: {
-    position: "absolute",
-    left: 20,
-    top: 30,
-    color: "black",
-    fontSize: 22,
-    fontWeight: 700,
+  reglabel: {
+    marginLeft: "10%",
+    marginTop: 15,
+    fontSize: 15,
+  },
+  emailinp: {
+    marginLeft: "7%",
+    width: "85%",
+    height: "10%",
+    borderWidth: 1,
+    borderColor: "rgb(224, 224, 224)",
+    borderRadius: 20,
+  },
+  email: {
+    height: "100%"
   }
 });
